261f6b7a70c67cbbe81a4af9d7679931e1659886
IshikaMeel <139564784+IshikaMeel@users.noreply.github.com>
2024-10-14 17:11:59 -0400

    Initial Commit

---
+++ ZeroTrace/ZeroTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZeroTraceApp.swift
+//  ZeroTrace
+//
+//  Created by Ishika Meel on 10/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ZeroTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
